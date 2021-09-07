6c76b9e9f2eaa66906eb6a7f5e4fc054a9de26f0
bwesterg <benton.westergaard@gmail.com>
2021-09-07 16:53:37 -0600

    Adds a delete button which removes a ski from the collection; changes persist.

--- ski-collection/src/components/SkiItem.js
+++ ski-collection/src/components/SkiItem.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-export default function SkiItem({brand, model, description, usage}) {
+export default function SkiItem({id, brand, model, description, usage, deleteSki}) {
+    
+    const handleClick = (event) => deleteSki(id)
+    
     return (
         <li className="ski-item">
             <h2>{brand}</h2>
             <h3>{model}</h3>
             <h4>{description}</h4>
             <h4>{usage}</h4>
+            <button onClick={handleClick} className="delete-button">Delete</button>
         </li>
     )
 }
