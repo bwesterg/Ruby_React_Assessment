ebe74e733c1611762a975c6498b4483b623edfac
bwesterg <benton.westergaard@gmail.com>
2021-09-09 16:58:39 -0400

    removed superfluous notes.

--- ski_collection_backend/config/routes.rb
+++ ski_collection_backend/config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   resources :users
   resources :skis
   post '/login', to: 'application#login'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
