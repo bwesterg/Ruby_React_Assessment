ab5d543b3cba6503b9642ddcc66e7093e0fe45f6
bwesterg <benton.westergaard@gmail.com>
2021-09-10 15:35:53 -0400

    auth decode debugging.

--- ski_collection_backend/app/controllers/application_controller.rb
+++ ski_collection_backend/app/controllers/application_controller.rb
@@ -1,4 +1,27 @@
 class ApplicationController < ActionController::API
+    
+    def current_user
+        auth_header = request.headers["Authorization"]
+        if auth_header
+            token = auth_header.split(" ")[1]
+            begin
+                user_id = JWT.decode(token, 'hallo')[0][:user_id]
+            rescue JWT::DecodeError
+                nil
+            end
+        end
+        @user = User.find(user_id)
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+
+    def authorized
+        render json: {message: "Please log in"}, status: :unauthorized unless logged_in?
+    end
+    
     def login
         @user = User.find_by(username: params[:username])
         if @user && @user.authenticate(params[:password])
